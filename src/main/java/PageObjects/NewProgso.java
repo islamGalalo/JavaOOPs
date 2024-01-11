b411d06a6fad33c905166fa1c08a028cc1f9efc7
islam.galal <Islam.Galal@BonianTech.Local>
2024-01-11 15:09:01 +0200

    programso

--- src/main/java/PageObjects/NewProgso.java
+++ src/main/java/PageObjects/NewProgso.java
@@ -1,2 +1,13 @@
-package PageObjects;public class NewProgso {
+package PageObjects;
+
+public class NewProgso {
+    public static void main(String[] args) {
+
+        Programso pros = new Programso() ;
+       pros.setAge(12);
+       pros.setName("sass");
+       pros.setExperience(8);
+       pros.setNationality("egyptian");
+       pros.printData();
+    }
 }
--- src/main/java/PageObjects/Programso.java
+++ src/main/java/PageObjects/Programso.java
@@ -1,2 +1,46 @@
-package PageObjects;public class programso {
+package PageObjects;
+
+public class Programso {
+    int age ;
+    int experience ;
+    String name ;
+    String nationality ;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public void printData () {
+        System.out.println(name);
+        System.out.println(nationality);
+        System.out.println(age);
+        System.out.println(experience);
+    }
 }
